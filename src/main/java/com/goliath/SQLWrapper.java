61738cba27643ffd73dcf6a09ef213a4eb75fcca
Arpaesis <arpaesis@gmail.com>
2019-03-27 06:05:37 -0400

    Cleaned imports.

--- src/main/java/com/goliath/SQLWrapper.java
+++ src/main/java/com/goliath/SQLWrapper.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import com.goliath.col.Col;
 import com.goliath.db.Database;
